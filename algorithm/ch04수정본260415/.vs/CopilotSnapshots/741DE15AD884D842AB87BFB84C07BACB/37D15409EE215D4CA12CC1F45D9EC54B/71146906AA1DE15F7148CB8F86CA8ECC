d2632ca56c45cce582d36b0a9939d54f0f9cb94e
ldh <ldonghee.dev@gmail.com>
2026-04-24 00:18:14 +0900

    7주차 알고리즘 및 중간고사 대비 md 파일 추가

---
+++ algorithm/ch04수정본260415/.vs/CopilotSnapshots/741DE15AD884D842AB87BFB84C07BACB/37D15409EE215D4CA12CC1F45D9EC54B/71146906AA1DE15F7148CB8F86CA8ECC
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "InsertLinkedList.h"
+
+int main(void) {
+	linkedList_h* L;
+	L = createLinkedList_h();
+	printf("(1) 공백 리스트 생성하기! \n");
+	printList(L);
+
+	printf("\n(2) 리스트에 [수] 노드 삽입하기! \n");
+	insertFirstNode(L, "수");
+	printList(L);
+
+	printf("\n(3) 리스트 마지막에 [금] 노드 삽입하기! \n");
+	insertLastNode(L, "금");
+	printList(L);
+
+	printf("\n(4) 리스트 첫 번째에 [월] 노드 삽입하기! \n");
+	insertFirstNode(L, "월");
+	printList(L);
+
+	printf("\n(5) [월] 노드 뒤에 [화] 삽입, [수] 노드 뒤에 [목] 삽입하기! \n");
+	insertMiddleNode(L, L->head, "화"); // 월 노드 뒤에 화 삽입
+	insertMiddleNode(L, L->head->link->link, "목"); // 화(link), 수(link->link) 뒤에 목 삽입
+	printList(L);
+
+	printf("\n(6) 리스트 공간을 해제하여 공백 리스트로 만들기! \n");
+	freeLinkedList_h(L);
+	printList(L);
+
+	//getchar();  
+	free(L);
+	return 0;
+}
---
+++ algorithm/ch04수정본260415/ch04_3/ex4_3.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+#include "CircularLinkedList.h"
+
+int main(void) 
+{
+	SetConsoleOutputCP(65001);//UTF-8,CP는 Code Page의 약자
+	SetConsoleCP(65001); // 입력도 필요하면 설정
+
+	linkedList_h* CL;
+	listNode* p;
+
+	CL = createLinkedList_h();		// 공백 원형 연결 리스트 생성
+	printf("(1) 원형 연결 리스트 생성하기! \n");
+	printList(CL);
+
+	printf("\n(2) 원형 연결 리스트에 [월] 노드 삽입하기! \n");
+	insertFirstNode(CL, "월");
+	printList(CL);
+
+	printf("\n(3) 원형 연결 리스트의 [월] 노드 뒤에 [수] 노드 삽입하기! \n");
+	p = searchNode(CL, "월"); insertMiddleNode(CL, p, "수");
+	printList(CL);
+
+	printf("\n(4) 원형 연결 리스트의 [수] 노드 뒤에 [금] 노드 삽입하기! \n");
+	p = searchNode(CL, "수"); insertMiddleNode(CL, p, "금");
+	printList(CL);
+
+	printf("\n(5) 원형 연결 리스트에서 [수] 노드 삭제하기! \n");
+	p = searchNode(CL, "수");  deleteNode(CL, p);
+	printList(CL);  	
+	
+	free(CL);
+	//getchar();
+
+	return 0;
+}
